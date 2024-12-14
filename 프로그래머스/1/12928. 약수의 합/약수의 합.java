cf9c059404016d79b901083a8698c6ca745b402c
taketheking <so4524@gmail.com>
2024-12-15 08:28:50 +0900

    [level 1] Title: 약수의 합, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -5,11 +5,11 @@ class Solution {
     public int solution(int n) {
         int sum = 0;
         
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=n/2; i++){
             if(n%i == 0){
                 sum += i;
             }
         }
-        return sum;
+        return sum+n;
     }
 }
