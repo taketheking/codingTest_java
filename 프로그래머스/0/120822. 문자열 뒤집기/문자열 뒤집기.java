afbff58f79d5b83d96127527e8a6278ed8bf0fcd
taketheking <so4524@gmail.com>
2024-12-15 06:18:35 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,11 +1,9 @@
 class Solution {
     public String solution(String my_string) {
-        char[] reverseCharArray = new char[my_string.length()];
-        
-        for(int i=0; i<my_string.length(); i++){
-            reverseCharArray[i] = my_string.charAt(my_string.length()-i-1);
-        }
-        
-        return String.valueOf(reverseCharArray);
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
+        sb.reverse();
+
+        return sb.toString();
     }
 }
