0285d1e5a33f9f2a3e92c04f263b29f4dab192bd
kim sewon <so4524@gmail.com>
2025-03-03 14:50:14 +0900

    [level 2] Title: 전화번호 목록, Time: 385.61 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,7 +1,7 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-    public boolean solution(String[] phone_book) {
+    public boolean solution2(String[] phone_book) {
         boolean answer = true;
         
         Arrays.sort(phone_book);
@@ -13,6 +13,27 @@ class Solution {
             }
         }
         
+        return answer;
+    }
+    
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        boolean answer = true;
+
+
+        for(int i = 0; i < phone_book.length; i++) {
+            String phone = phone_book[i];
+
+             if(i+1 < phone_book.length && phone_book[i+1].length() >= phone.length() ) {
+
+                 String str = phone_book[i+1].substring(0,phone.length());
+
+                 if(str.equals(phone)) {
+                     answer = false;
+                 }
+             }
+
+        }
         return answer;
     }
 }
